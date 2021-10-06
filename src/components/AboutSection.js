5b99fe71e39bedfec9e345905ff7cfd1aad4feed
Yarin Matmoni <yarinmatmoni@cs.colman.ac.il>
2021-10-07 00:20:02 +0300

    Add animation for about section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,6 +2,7 @@ import React from 'react';
 import home1 from "../img/home1.png";
 import {About,Description,Hide,Image} from "../Style";
 import {motion} from "framer-motion";
+import {titleAnim,fade,phtoAnim} from "../animation";
 
 function AboutSection() {
     return (
@@ -9,23 +10,23 @@ function AboutSection() {
             <Description>
                 <motion.div>
                     <Hide>
-                        <motion.h2>We work to make</motion.h2>
+                        <motion.h2 variants={titleAnim}>We work to make</motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2>your <span>dreams</span> come</motion.h2>
+                        <motion.h2 variants={titleAnim}>your <span>dreams</span> come</motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2>true.</motion.h2>
+                        <motion.h2 variants={titleAnim}>true.</motion.h2>
                     </Hide>
                 </motion.div>
-                <p>
+                <motion.p variants={fade}>
                     Contect us for any photography or videography ideas that you have.
                     We have professionals with amazing skills.
-                </p>
-                <button>Contect Us</button>
+                </motion.p>
+                <motion.button variants={fade}>Contect Us</motion.button>
             </Description>
             <Image>
-                <img src={home1} alt="guy with a camera"></img>
+                <motion.img variants={phtoAnim} src={home1} alt="guy with a camera"></motion.img>
             </Image>
         </About>
     )

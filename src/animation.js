6854603d9bb536702fa689a91950a81ca5378803
Yarin Matmoni <yarinmatmoni@cs.colman.ac.il>
2021-10-07 03:23:20 +0300

    Add animation for our work page

--- src/animation.js
+++ src/animation.js
@@ -8,14 +8,13 @@ export const pageAnimation = {
         opacity: 1,
         y:0,
         transition:{
-             duration: 1,
+             duration: 0.5,
              when:"beforeChildren",
              staggerChildren: 0.25,
         }
     },
     exit:{
         opacity: 0,
-        y:300,
         transition:{
             duration: 0.5,
        }
@@ -61,4 +60,44 @@ export const phtoAnim = {
             ease:"easeOut",
         },
     },
+};
+
+export const lineAnim = {
+    hidden:{
+        width: '0%'
+    },
+    show:{
+        width: '100%',
+        transition:{
+            duration:1,
+        },
+    },
+};
+
+export const slider = {
+    hidden:{
+        x:'-130%',
+        skew:"45deg",
+    },
+    show:{
+        x:'100%',
+        skew:"0deg",
+        transition: {
+            ease:"easeOut",
+            duration:1,
+        },
+    },
+}
+
+export const sliderContiner = {
+    hidden:{
+        opacity: 1,
+    },
+    show:{
+        opacity: 1,
+        transition: {
+            ease:"easeOut",
+            staggerChildren:0.15,
+        },
+    }
 };

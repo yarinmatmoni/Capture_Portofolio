7a61e55a8ab0aa659294dde43a02d2d885c6de2a
Yarin Matmoni <yarinmatmoni@cs.colman.ac.il>
2021-10-09 08:21:56 +0300

    Add nav line animation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router';
 //Import style
 import styled from 'styled-components';
+import {motion} from "framer-motion";
 
 function Nav() {
+    const {pathname} = useLocation();
     return (
         <StyledNav>
             <h1><Link id="logo" to="/">Capture</Link></h1>
             <ul>
                 <li>
                     <Link to="/">1. About Us</Link>
+                    <Line transition={{duration:0.75}} initial={{width:"0%"}} animate={{width: pathname === "/" ? "45%" : "0%"}}/>
                 </li>
                 <li>
                     <Link to="/work">2. Our Work</Link>
+                    <Line transition={{duration:0.75}} initial={{width:"0%"}} animate={{width: pathname === "/work" ? "45%" : "0%"}}/>
                 </li>
                 <li>
                     <Link to="/contact">3. Contact Us</Link>
+                    <Line transition={{duration:0.75}} initial={{width:"0%"}} animate={{width: pathname === "/contact" ? "45%" : "0%"}}/>
                 </li>
             </ul>
         </StyledNav>
@@ -70,6 +76,19 @@ const StyledNav = styled.div`
         li{
             padding: 0;
         }
+    }
+`;
+
+const Line = styled(motion.div)`
+    height: 0.3rem;
+    background: #23d997;
+    width: 0%;
+    position: absolute;
+    bottom: -80%;
+    left: 60%;
+
+    @media (max-width : 1300px){
+        left: 0%;
     }
 `;
 
